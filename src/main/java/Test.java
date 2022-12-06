f05128ae89fe7e72c2c3e2053dbee17469913bc2
Nithish <nithishgwd007@gmail.com>
2022-12-06 06:42:32 +0530

    CHNAGED JAVA FILE

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("HI WELOCOME TO CI/CD");
+        System.out.println("HI WELOCOME TO CI/CD TO MASHOOD");
     }
 }
